1fd9ac5cd0f02323b0445c0e20e3923089307e9f
Peter Carroll <petelc1966@hotmail.com>
2026-01-18 16:31:32 -0500

    Fixed PR By ID endpoint

--- src/Apex.API.UseCases/ProjectRequests/GetById/GetProjectRequestByIdQuery.cs
+++ src/Apex.API.UseCases/ProjectRequests/GetById/GetProjectRequestByIdQuery.cs
@@ -1,13 +1,12 @@
-using MediatR;
-using Ardalis.Result;
 using Apex.API.Core.ValueObjects;
+using Apex.API.UseCases.ProjectRequests.DTOs;
+using Ardalis.Result;
+using MediatR;
 
 namespace Apex.API.UseCases.ProjectRequests.GetById;
 
 /// <summary>
-/// Query to get a ProjectRequest by ID
+/// Query to get a single project request by ID
 /// </summary>
-public record GetProjectRequestByIdQuery(ProjectRequestId ProjectRequestId)
+public record GetProjectRequestByIdQuery(ProjectRequestId ProjectRequestId) 
     : IRequest<Result<ProjectRequestDto>>;
-
-
